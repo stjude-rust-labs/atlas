028e39554f6c363965b45be017addbe98e87a79e
Michael Macias <michael.macias@stjude.org>
2022-02-28 11:37:02 -0600

    migrations: Add configurations

---
+++ migrations/20220228172952_create_configurations.sql
@@ -0,0 +1,12 @@
+create table configurations (
+    id serial primary key,
+
+    annotation_id integer not null,
+
+    feature_type text not null,
+    feature_name text not null,
+
+    created_at timestamptz not null default now(),
+
+    foreign key (annotation_id) references annotations (id)
+);
--- tests/sql/seeds.sql
+++ tests/sql/seeds.sql
@@ -10,6 +10,11 @@ values
     ('GENCODE 39', 'GRCh38.p13'),
     ('GENCODE 19', 'GRCh37.p13');
 
+insert into configurations
+    (annotation_id, feature_type, feature_name)
+values
+    (1, 'exon', 'gene_name');
+
 insert into counts
     (sample_id, annotation_id, data_type)
 values

f567bcc7343504eb82696080e26c28b5bf29a467
Michael Macias <michael.macias@stjude.org>
2022-05-20 10:00:34 -0500

    cli: Add descriptions to ImportConfig fields

--- src/cli.rs
+++ src/cli.rs
@@ -19,27 +19,38 @@ pub enum Commands {
 
 #[derive(Debug, Parser)]
 pub struct ImportConfig {
+    /// The PostgreSQL database connection URL.
     #[clap(long, env)]
     pub database_url: String,
 
+    /// The name of the annotations, e.g., "GENCODE 40", etc.
     #[clap(long)]
     pub annotations_name: String,
 
+    /// The assembly name of the genome used to create the annotations, e.g., "GRCh37",
+    /// "GRCh38.p13", etc.
     #[clap(long)]
     pub annotations_genome_build: String,
 
+    /// The type of feature used in the annotations, e.g., "exon", "gene", etc.
     #[clap(long)]
     pub feature_type: String,
 
+    /// The display name of the feature, e.g., "gene_name", "gene_id", etc.
     #[clap(long)]
     pub feature_name: String,
 
+    /// The sample name.
     #[clap(long)]
     pub sample_name: String,
 
+    /// The technique process used to sequence the given sample, e.g., "RNA-Seq", etc.
     #[clap(long)]
     pub data_type: String,
 
+    /// The input source.
+    ///
+    /// It is assumed to be the output of htseq-count, i.e., a tab-delimited text file.
     pub src: PathBuf,
 }
 

33e4d4292a24a8ce5a6f96a61e85b8f55c4b1eb5
Michael Macias <michael.macias@stjude.org>
2022-05-20 10:24:48 -0500

    commands/import: Discard htseq-count metadata when reading counts

--- src/commands/import.rs
+++ src/commands/import.rs
@@ -295,13 +295,18 @@ async fn read_feature_counts<R>(reader: &mut R) -> anyhow::Result<HashMap<String
 where
     R: AsyncBufRead + Unpin,
 {
-    const TAB: char = '\t';
+    const DELIMITER: char = '\t';
+    const HTSEQ_COUNT_META_PREFIX: &str = "__";
 
     let mut lines = reader.lines();
     let mut counts = HashMap::new();
 
     while let Some(line) = lines.next_line().await? {
-        if let Some((raw_name, raw_count)) = line.split_once(TAB) {
+        if let Some((raw_name, raw_count)) = line.split_once(DELIMITER) {
+            if raw_name.starts_with(HTSEQ_COUNT_META_PREFIX) {
+                break;
+            }
+
             let count = raw_count.parse()?;
             counts.insert(raw_name.into(), count);
         }
@@ -316,7 +321,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_read_feature_counts() -> anyhow::Result<()> {
-        let data = b"feature_1\t8\nfeature_2\t13\n";
+        let data = b"feature_1\t8\nfeature_2\t13\n__no_feature\t0";
 
         let mut reader = &data[..];
         let counts = read_feature_counts(&mut reader).await?;

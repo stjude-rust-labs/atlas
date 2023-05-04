2923ced13140cf2fb2e59cb230d22883d5802d9b
Michael Macias <michael.macias@stjude.org>
2023-05-03 19:23:06 -0500

    counts/reader/htseq_count: Add line parser

--- src/counts/reader/htseq_count.rs
+++ src/counts/reader/htseq_count.rs
@@ -1,5 +1,6 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, num};
 
+use thiserror::Error;
 use tokio::io::AsyncBufRead;
 
 use super::read_line;
@@ -8,7 +9,6 @@ pub async fn read_counts<R>(reader: &mut R) -> anyhow::Result<HashMap<String, u6
 where
     R: AsyncBufRead + Unpin,
 {
-    const DELIMITER: char = '\t';
     const HTSEQ_COUNT_META_PREFIX: &str = "__";
 
     let mut line = String::new();
@@ -21,20 +21,31 @@ where
             break;
         }
 
-        if let Some((raw_name, raw_count)) = line.split_once(DELIMITER) {
-            if raw_name.starts_with(HTSEQ_COUNT_META_PREFIX) {
-                break;
-            }
+        let (name, count) = parse_line(&line)?;
 
-            let count = raw_count.parse()?;
-
-            counts.insert(raw_name.into(), count);
-        } else {
-            anyhow::bail!("invalid feature count line");
+        if name.starts_with(HTSEQ_COUNT_META_PREFIX) {
+            break;
         }
+
+        counts.insert(name, count);
     }
 
     Ok(counts)
+}
+
+#[derive(Debug, Error, Eq, PartialEq)]
+pub enum ParseError {
+    #[error("invalid input")]
+    Invalid,
+    #[error("invalid count")]
+    InvalidCount(num::ParseIntError),
+}
+
+fn parse_line(s: &str) -> Result<(String, u64), ParseError> {
+    const DELIMITER: char = '\t';
+    let (raw_name, raw_count) = s.split_once(DELIMITER).ok_or(ParseError::Invalid)?;
+    let count = raw_count.parse().map_err(ParseError::InvalidCount)?;
+    Ok((raw_name.into(), count))
 }
 
 #[cfg(test)]
@@ -56,10 +67,17 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test_read_counts_with_invalid_line() -> anyhow::Result<()> {
-        let data = b"feature_1\t8\nfeature_2  13\n";
-        let mut reader = &data[..];
-        assert!(read_counts(&mut reader).await.is_err());
-        Ok(())
+    async fn test_parse_line() {
+        assert_eq!(
+            parse_line("feature_1\t8"),
+            Ok((String::from("feature_1"), 8))
+        );
+
+        assert_eq!(parse_line("feature_2 13"), Err(ParseError::Invalid));
+
+        assert!(matches!(
+            parse_line("feature_3\tone"),
+            Err(ParseError::InvalidCount(_))
+        ));
     }
 }

c7e29dc6a15d27e6c747b57bdadf8e4168966820
Michael Macias <michael.macias@stjude.org>
2023-05-05 14:23:28 -0500

    server/error: Derive thiserror::Error

--- src/server/error.rs
+++ src/server/error.rs
@@ -1,16 +1,17 @@
-use std::fmt;
-
 use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
 };
 use tracing::error;
 
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
+    #[error("not found")]
     NotFound,
-    Sqlx(sqlx::Error),
-    Anyhow(anyhow::Error),
+    #[error("database error")]
+    Sqlx(#[from] sqlx::Error),
+    #[error("internal server error")]
+    Anyhow(#[from] anyhow::Error),
 }
 
 impl Error {
@@ -22,40 +23,14 @@ impl Error {
     }
 }
 
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::NotFound => f.write_str("not found"),
-            Self::Sqlx(_) => f.write_str("database error"),
-            Self::Anyhow(_) => f.write_str("internal server error"),
-        }
-    }
-}
-
 impl IntoResponse for Error {
     fn into_response(self) -> Response {
-        match self {
-            Self::Sqlx(ref e) => {
-                error!("{:?}", e);
-            }
-            Self::Anyhow(ref e) => {
-                error!("{:?}", e);
-            }
+        match &self {
+            Self::Sqlx(e) => error!("{e:?}"),
+            Self::Anyhow(e) => error!("{e:?}"),
             _ => {}
         }
 
         (self.status_code(), self.to_string()).into_response()
-    }
-}
-
-impl From<sqlx::Error> for Error {
-    fn from(e: sqlx::Error) -> Self {
-        Self::Sqlx(e)
-    }
-}
-
-impl From<anyhow::Error> for Error {
-    fn from(e: anyhow::Error) -> Self {
-        Self::Anyhow(e)
     }
 }

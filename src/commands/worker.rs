eb0d45039b25b21ab1791e65b76a896ca4675575
Michael Macias <michael.macias@stjude.org>
2023-08-02 15:23:53 -0500

    commands/worker: Add plot message handler

--- src/commands/worker.rs
+++ src/commands/worker.rs
@@ -3,7 +3,10 @@ use std::time::Duration;
 use sqlx::postgres::PgPoolOptions;
 use tracing::info;
 
-use crate::{cli::WorkerConfig, queue::Queue};
+use crate::{
+    cli::WorkerConfig,
+    queue::{task::plot, Message, Queue},
+};
 
 const POLL_INTERVAL: Duration = Duration::from_secs(1);
 
@@ -11,14 +14,21 @@ pub async fn worker(config: WorkerConfig) -> anyhow::Result<()> {
     let pool = PgPoolOptions::new().connect(&config.database_url).await?;
     sqlx::migrate!().run(&pool).await?;
 
-    let queue = Queue::new(pool);
+    let queue = Queue::new(pool.clone());
 
     info!("worker initialized");
 
     loop {
         if let Some(task) = queue.pull_front().await? {
             info!(id = ?task.id, "received task");
-            dbg!(task.id);
+
+            match task.message.0 {
+                Message::Noop => {}
+                Message::Plot(configuration_id) => {
+                    plot(&pool, configuration_id).await?;
+                }
+            }
+
             queue.success(task.id).await?;
         }
 

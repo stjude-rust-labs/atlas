f5fc5cc4eb7e1ffb076fe37307a36fc5dbf39add
Michael Macias <michael.macias@stjude.org>
2023-08-02 14:50:18 -0500

    server/analyses: Return 404 if configuration ID does not exist

--- src/server/analyses.rs
+++ src/server/analyses.rs
@@ -6,7 +6,7 @@ use axum::{
 use serde::Serialize;
 use uuid::Uuid;
 
-use super::Context;
+use super::{Context, Error};
 
 pub fn router() -> Router<Context> {
     Router::new().route("/analyses/plot/:configuration_id", get(plot))
@@ -33,10 +33,43 @@ async fn plot(
     State(ctx): State<Context>,
     Path(configuration_id): Path<i32>,
 ) -> super::Result<Json<PlotResponse>> {
-    use crate::queue::Message;
+    use crate::{queue::Message, store::configuration};
+
+    if !dbg!(configuration::exists(&ctx.pool, configuration_id).await?) {
+        return Err(Error::NotFound);
+    }
 
     let message = Message::Plot(configuration_id);
     let id = ctx.queue.push_back(message).await?;
 
     Ok(Json(PlotResponse { id }))
+}
+
+#[cfg(test)]
+mod tests {
+    use axum::body::Body;
+    use hyper::{Request, StatusCode};
+    use sqlx::PgPool;
+    use tower::ServiceExt;
+
+    use super::*;
+    use crate::Queue;
+
+    fn app(pool: PgPool) -> Router {
+        let queue = Queue::new(pool.clone());
+        router().with_state(Context { pool, queue })
+    }
+
+    #[sqlx::test]
+    async fn test_plot_with_invalid_configuration_id(pool: PgPool) -> anyhow::Result<()> {
+        let request = Request::builder()
+            .uri("/analyses/plot/-1")
+            .body(Body::empty())?;
+
+        let response = app(pool).oneshot(request).await?;
+
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+
+        Ok(())
+    }
 }
--- src/store/configuration.rs
+++ src/store/configuration.rs
@@ -1,10 +1,19 @@
-use sqlx::{Postgres, Transaction};
+use sqlx::{PgPool, Postgres, Transaction};
 
 use super::StrandSpecification;
 
 #[derive(Debug)]
 pub struct Configuration {
     pub id: i32,
+}
+
+pub async fn exists(pool: &PgPool, id: i32) -> sqlx::Result<bool> {
+    sqlx::query_scalar!(
+        r#"select exists(select 1 from configurations where id = $1) as "exists!""#,
+        id
+    )
+    .fetch_one(pool)
+    .await
 }
 
 pub async fn find_or_create_configuration(
@@ -42,11 +51,31 @@ mod tests {
     use sqlx::PgPool;
 
     use super::*;
+    use crate::store::annotations::find_or_create_annotations;
+
+    #[sqlx::test]
+    async fn test_exists(pool: PgPool) -> anyhow::Result<()> {
+        assert!(!exists(&pool, 1).await?);
+
+        let mut tx = pool.begin().await?;
+
+        let gencode_40 = find_or_create_annotations(&mut tx, "GENCODE 40", "GRCh38.p13").await?;
+        let configuration = find_or_create_configuration(
+            &mut tx,
+            gencode_40.id,
+            "gene",
+            "gene_name",
+            StrandSpecification::Reverse,
+        )
+        .await?;
+
+        assert!(!exists(&pool, configuration.id).await?);
+
+        Ok(())
+    }
 
     #[sqlx::test]
     async fn test_find_or_create_configuration(pool: PgPool) -> anyhow::Result<()> {
-        use crate::store::annotations::find_or_create_annotations;
-
         let mut tx = pool.begin().await?;
 
         let gencode_21 = find_or_create_annotations(&mut tx, "GENCODE 21", "GRCh38").await?;

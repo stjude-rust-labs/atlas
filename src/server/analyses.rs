902fdb541c03b4b9ab8f010f450d5f2ee5a433b7
Michael Macias <michael.macias@stjude.org>
2023-08-02 14:16:06 -0500

    server/analyses: Return task ID in response

--- src/server/analyses.rs
+++ src/server/analyses.rs
@@ -1,13 +1,20 @@
 use axum::{
     extract::{Path, State},
     routing::get,
-    Router,
+    Json, Router,
 };
+use serde::Serialize;
+use uuid::Uuid;
 
 use super::Context;
 
 pub fn router() -> Router<Context> {
     Router::new().route("/analyses/plot/:configuration_id", get(plot))
+}
+
+#[derive(Serialize)]
+struct PlotResponse {
+    id: Uuid,
 }
 
 /// Submits a task to perform dimension reduction on all samples in a configuation.
@@ -18,15 +25,18 @@ pub fn router() -> Router<Context> {
         ("configuration-id" = i32, Path, description = "Configuration ID"),
     ),
     responses(
-        (status = OK, description = "A task to plot the given configuration was submitted"),
+        (status = OK, description = "The ID of the task submitted"),
         (status = NOT_FOUND, description = "The configuration ID does not exist"),
     ),
 )]
-async fn plot(State(ctx): State<Context>, Path(configuration_id): Path<i32>) -> super::Result<()> {
+async fn plot(
+    State(ctx): State<Context>,
+    Path(configuration_id): Path<i32>,
+) -> super::Result<Json<PlotResponse>> {
     use crate::queue::Message;
 
     let message = Message::Plot(configuration_id);
-    ctx.queue.push_back(message).await?;
+    let id = ctx.queue.push_back(message).await?;
 
-    Ok(())
+    Ok(Json(PlotResponse { id }))
 }

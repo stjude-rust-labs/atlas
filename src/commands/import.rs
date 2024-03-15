3936d7b9f2003ecd686c1b63e5b99f958f33330e
Michael Macias <michael.macias@stjude.org>
2024-03-15 14:48:39 -0500

    commands/import: Extract single sample importer

--- src/commands/import.rs
+++ src/commands/import.rs
@@ -1,22 +1,14 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, path::Path};
 
-use sqlx::postgres::PgPoolOptions;
+use sqlx::{postgres::PgPoolOptions, Postgres, Transaction};
 use tokio::{fs::File, io::BufReader};
 use tracing::info;
 
-use crate::cli::ImportConfig;
+use crate::{cli::ImportConfig, counts::Format, store::StrandSpecification};
 
 pub async fn import(config: ImportConfig) -> anyhow::Result<()> {
-    use crate::{
-        counts::reader::read_counts,
-        store::{
-            annotations::find_or_create_annotations,
-            configuration::find_or_create_configuration,
-            count::create_counts,
-            feature::{create_features, find_features},
-            run::{create_run, run_exists},
-            sample::find_or_create_sample,
-        },
+    use crate::store::{
+        annotations::find_or_create_annotations, configuration::find_or_create_configuration,
     };
 
     let pool = PgPoolOptions::new().connect(&config.database_url).await?;
@@ -44,39 +36,76 @@ pub async fn import(config: ImportConfig) -> anyhow::Result<()> {
 
     info!(id = configuration.id, "loaded configuration");
 
-    let sample = find_or_create_sample(&mut tx, &config.sample_name).await?;
-
-    info!(id = sample.id, "loaded sample");
-
-    if run_exists(&mut tx, configuration.id, sample.id).await? {
-        tx.rollback().await?;
-        anyhow::bail!("run already exists for the sample and configuration");
-    }
-
-    let mut features = find_features(&mut tx, configuration.id).await?;
-
-    info!("loaded {} feature", features.len());
-
-    let mut reader = File::open(&config.src).await.map(BufReader::new)?;
-    let counts = read_counts(
-        &mut reader,
+    let result = import_one(
+        &mut tx,
+        &config.src,
+        configuration.id,
+        &config.sample_name,
         config.format,
         &config.feature_name,
         config.strand_specification,
+        &config.data_type,
     )
-    .await?;
+    .await;
+
+    match result {
+        Ok(()) => tx.commit().await?,
+        Err(e) => {
+            tx.rollback().await?;
+            anyhow::bail!("{e}");
+        }
+    }
+
+    Ok(())
+}
+
+async fn import_one<P>(
+    tx: &mut Transaction<'_, Postgres>,
+    src: P,
+    configuration_id: i32,
+    sample_name: &str,
+    format: Option<Format>,
+    feature_name: &str,
+    strand_specification: StrandSpecification,
+    data_type: &str,
+) -> anyhow::Result<()>
+where
+    P: AsRef<Path>,
+{
+    use crate::{
+        counts::reader::read_counts,
+        store::{
+            count::create_counts,
+            feature::{create_features, find_features},
+            run::{create_run, run_exists},
+            sample::find_or_create_sample,
+        },
+    };
+
+    let sample = find_or_create_sample(tx, sample_name).await?;
+
+    info!(id = sample.id, "loaded sample");
+
+    if run_exists(tx, configuration_id, sample.id).await? {
+        anyhow::bail!("run already exists for the sample and configuration");
+    }
+
+    let mut features = find_features(tx, configuration_id).await?;
+
+    info!("loaded {} feature", features.len());
+
+    let mut reader = File::open(src).await.map(BufReader::new)?;
+    let counts = read_counts(&mut reader, format, feature_name, strand_specification).await?;
 
     if features.is_empty() {
         let mut names = HashSet::new();
         names.extend(counts.keys().cloned());
-        features = create_features(&mut tx, configuration.id, &names).await?;
+        features = create_features(tx, configuration_id, &names).await?;
         info!("created {} features", features.len());
     }
 
-    let run = create_run(&mut tx, configuration.id, sample.id, &config.data_type).await?;
-    create_counts(&mut tx, run.id, &features, &counts).await?;
-
-    tx.commit().await?;
+    let run = create_run(tx, configuration_id, sample.id, data_type).await?;
+    create_counts(tx, run.id, &features, &counts).await?;
 
     Ok(())
 }

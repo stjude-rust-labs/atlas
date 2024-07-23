31c8e6e7933fab46be19ee91c1593dc630752720
Michael Macias <michael.macias@stjude.org>
2024-07-23 13:21:30 -0500

    Update to sqlx 0.8.0

--- src/store/strand_specification.rs
+++ src/store/strand_specification.rs
@@ -1,6 +1,5 @@
 use clap::ValueEnum;
 use serde::Serialize;
-use sqlx::postgres::{PgHasArrayType, PgTypeInfo};
 
 #[derive(ValueEnum, Clone, Serialize, Copy, Debug, Eq, PartialEq, sqlx::Type, utoipa::ToSchema)]
 #[serde(rename_all = "lowercase")]
@@ -9,10 +8,4 @@ pub enum StrandSpecification {
     None,
     Forward,
     Reverse,
-}
-
-impl PgHasArrayType for StrandSpecification {
-    fn array_type_info() -> PgTypeInfo {
-        PgTypeInfo::with_name("_strand_specification")
-    }
 }

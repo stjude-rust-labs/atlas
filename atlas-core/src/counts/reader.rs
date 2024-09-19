5e24220e8213076fea5b3cdf1d4197cdead14a85
Michael Macias <michael.macias@stjude.org>
2024-09-19 15:04:41 -0500

    core/counts/reader: Add format detection

--- atlas-core/src/counts/reader.rs
+++ atlas-core/src/counts/reader.rs
@@ -4,21 +4,52 @@ mod star;
 
 use std::io::{self, BufRead};
 
+use tracing::warn;
+
 pub use self::format::Format;
 use crate::StrandSpecification;
 
 pub fn read<R>(
     reader: &mut R,
-    format: Format,
+    format: Option<Format>,
     feature_name: &str,
     strand_specification: StrandSpecification,
 ) -> io::Result<Vec<(String, u64)>>
 where
     R: BufRead,
 {
+    let detected_format = detect_format(reader)?;
+
+    if let Some(expected_format) = format {
+        if detected_format != expected_format {
+            warn!(
+                expected = ?expected_format,
+                actual = ?detected_format,
+                "format mismatch"
+            );
+        }
+    }
+
+    let format = format.unwrap_or(detected_format);
+
     match format {
         Format::HtseqCount => htseq_count::read(reader),
         Format::Star => star::read(reader, feature_name, strand_specification),
+    }
+}
+
+fn detect_format<R>(reader: &mut R) -> io::Result<Format>
+where
+    R: BufRead,
+{
+    const STAR_FORMAT_PREFIX: &[u8] = b"# gene-model:";
+
+    let src = reader.fill_buf()?;
+
+    if src.starts_with(STAR_FORMAT_PREFIX) {
+        Ok(Format::Star)
+    } else {
+        Ok(Format::HtseqCount)
     }
 }
 
@@ -48,6 +79,21 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_detect_format() -> io::Result<()> {
+        fn t(mut src: &[u8], expected: Format) -> io::Result<()> {
+            let actual = detect_format(&mut src)?;
+            assert_eq!(actual, expected);
+            Ok(())
+        }
+
+        t(b"# gene-model: GENCODE v46\n", Format::Star)?;
+        t(b"f0\t8\n", Format::HtseqCount)?;
+        t(b"atlas\n", Format::HtseqCount)?;
+
+        Ok(())
+    }
 
     #[test]
     fn test_read_line() -> io::Result<()> {
